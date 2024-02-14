8033012c1044b8aba5018f8e042d53631d2057be
Yumiria <154666142+Yumiara@users.noreply.github.com>
2024-02-14 08:00:34 +0700

    Update kcehCemaG.lua

--- kcehCemaG.lua
+++ kcehCemaG.lua
@@ -34,6 +34,8 @@ end
 wait()
 if placeId == 3475397644 or placeId == 5391312853 or placeId == 3752680052 or placeId == 4174118306 or placeId == 3475419198 or placeId == 3475422608 or placeId == 4601778915 or placeId == 4869039553 or placeId == 5777228223 or placeId == 3623549100 or placeId == 3737848045 or placeId == 3487210751 or placeId == 4728805070 or placeId == 5777228223 then
     getgenv().ScriptName = "Dragon Adventure" --Normal World
+else
+    getgenv().ScriptName = "Universal"
 end
 task.wait()
 if getgenv().ScriptName == "The Mimic" then
