fc5f712602c52469216851d04805648740ed5b81
Yumiria <154666142+Yumiara@users.noreply.github.com>
2024-04-15 11:57:11 +0700

    Update ForceKick.lua

--- ForceKick.lua
+++ ForceKick.lua
@@ -1,1 +1,5 @@
 getgenv().ForceKick = false
+local MonaHWID = game:GetService("RbxAnalyticsService").GetClientId
+if MonaHWID == "e07e8beb-3e78-47f6-8f2a-93137d3de205" then
+    game.Players.LocalPlayer:Kick("Never Lose Your Smile")
+end
